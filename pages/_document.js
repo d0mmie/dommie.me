0d7e5f64092275e0da5ac00d148e2609bd1f4c13
d0mmie <st234@outlook.co.th>
2019-04-07 19:29:40 +0700

    added title

--- pages/_document.js
+++ pages/_document.js
@@ -10,11 +10,12 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <meta
-          name='viewport'
-          content='width=device-width, initial-scale=1'
-          key='viewport'
-        />
+          <title>{'DOMMIE - PORTFOLIO'}</title>
+          <meta
+            name='viewport'
+            content='width=device-width, initial-scale=1'
+            key='viewport'
+          />
           <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css' />
           <link href='https://unpkg.com/ionicons@4.5.5/dist/css/ionicons.min.css' rel='stylesheet' />
           <link rel='stylesheet' href='/static/style.css' />
